b95d83cd318b0eab5ebc590b863a988aa455f983
Yang Ye <shrine79@gmail.com>
2017-09-26 12:29:50 +1000

    added saving function to gui

--- src/baxter_tictactoe/scripts/camera_gui.py
+++ src/baxter_tictactoe/scripts/camera_gui.py
@@ -14,6 +14,10 @@ from PIL import Image
 from PIL import ImageTk, ImageDraw
 import numpy as np
 from PIL._imaging import outline
+import glob
+import os
+from reportlab.platypus.flowables import ImageAndFlowables
+
 
 
 class StatusBar(tk.Frame):
@@ -43,6 +47,7 @@ class MainWin(object):
     
     def __init__(self, parent):
         #self.camera = Cam()
+        self.max_screen_res = (1024, 600)
         #============================== frames =========================================
         self.frameA = tk.Frame(parent, highlightbackground="green", highlightcolor="green", highlightthickness=2)
         self.frameA.pack(anchor=tk.W, padx = 10, pady = 10)
@@ -53,18 +58,18 @@ class MainWin(object):
         self.stbar =  StatusBar(parent)
         self.stbar.pack(side="bottom", fill='x')
         #============================= panels ==========================================
+        self.res_mode = 2
         data = np.zeros((400, 640, 3), dtype=np.uint8) 
         #data = np.zeros(self.camera.cam_proxy.resolution + (3,), dtype=np.uint8) 
         self.imageA = Image.fromarray(data, 'RGB')
-        self.imageA = ImageTk.PhotoImage(self.imageA)
-        self.panelA = tk.Label(self.frameA, image=self.imageA)
+        self.imageATk = ImageTk.PhotoImage(self.imageA)
+        self.panelA = tk.Label(self.frameA, image=self.imageATk)
         self.panelA.pack(side=tk.LEFT, padx = 10, pady = 10)
         
         self.imageB = Image.fromarray(data, 'RGB')
-        draw = ImageDraw.Draw(self.imageB)
-        draw.rectangle( ((30, 20), (550, 350)), outline="red")
-        self.imageB = ImageTk.PhotoImage(self.imageB)
-        self.panelB = tk.Label(self.frameA, image=self.imageB)
+        self.draw_box()
+        self.imageBTk = ImageTk.PhotoImage(self.imageB)
+        self.panelB = tk.Label(self.frameA, image=self.imageBTk)
         self.panelB.pack(side=tk.LEFT, padx = 10, pady = 10)
         
         #============================= Buttons =====================================
@@ -87,11 +92,31 @@ class MainWin(object):
         down_btn = tk.Button(self.frameB, text="down", command=self.snapshot, width=3)
         down_btn.grid(row=1, column=5,padx=10, pady=10)
         
+    def draw_box(self):
+        # calculate bounding box pixel size in window display
+        rect_w, rect_h = tuple(n * pow(0.75, 2) for n in self.max_screen_res)
+        ori_x = (640 - rect_w)//2 
+        ori_y = (400 - rect_h)//2
+        
+        draw = ImageDraw.Draw(self.imageB)
+        draw.rectangle( ((ori_x, ori_y), (ori_x + rect_w, ori_y + rect_h)), outline="red")
+    
     def snapshot(self):
-        pass
+        '''
+        take snapshot and save file
+        '''
+        #save to files
+        self.save_file()
+        
+        #update panelA with captured image
+        self.panelA.config(image=self.imageBTk)
+        
     
     def move_arm(self):
-        pass
+        '''
+        move baxter arm to position for snapshot
+        '''
+        
         
     def select_path(self):
         global path, prefix
@@ -103,6 +128,43 @@ class MainWin(object):
         global path, prefix
         prefix = tkSimpleDialog.askstring(title="select a prefix", prompt="the saved file will look like " + prefix + "_xx")
         self.stbar.update()
+    
+    def save_file(self):
+        '''
+        save file to directory specified with prefix to file name. 
+        The save file name would be like [prefix]_xx, where xx denotes serial number. 
+        '''
+        global path, prefix
+        # find the pre-existing file with largest serial number
+        last_sn = 0
+        head = None
+        tail = ".png" # save a .png file by default
+        fullstr = path + '/' + prefix + '_[0-9]*.png'
+        for fn in glob.glob(fullstr):
+            full_name = os.path.split(fn)[1] 
+            head, tail = os.path.splitext(full_name)
+            sn = head.split('_')[1]
+            if last_sn < int(sn): 
+                last_sn = int(sn)
+        
+        new_fn = prefix + '_' + str(last_sn + 1).zfill(2) + tail
+        new_full_fn = path + '/' + new_fn
+        print "saved as" +  new_full_fn
+        
+        #crop and resize image for baxter head screen resolution 1024 x 600
+        rect_w, rect_h = tuple(int(n * pow(0.75, self.res_mode)) for n in self.max_screen_res)
+        ori_x = (1280 - rect_w)//2
+        ori_y = (800 - rect_h)//2 
+        imageB_crop = self.imageB.crop((ori_x, ori_y, ori_x+rect_w, ori_y+rect_h)).resize(self.max_screen_res)
+        #make a stamp
+        draw = ImageDraw.Draw(imageB_crop)
+        draw.text( (20, 20), new_fn, anchor='w')
+        
+        imageB_crop.save(new_full_fn)
+        
+        
+    
+    
 
 class Cam(object):
     """
@@ -110,8 +172,11 @@ class Cam(object):
     """
     def __init__(self):
         self.cam_proxy = CameraController("left_hand_camera")
+        
         # default resolution is 640 x 400
-        self.cam_proxy.resolution(CameraController.MODES[2])
+        # MODES = [(1280, 800), (960, 600), (640, 400), (480, 300), (384, 240), (320, 200)]
+        self.res_mode = 2
+        self.cam_proxy.resolution(CameraController.MODES[self.res_mode])
         # open camera by default
         self.cam_proxy.open()
         
@@ -141,8 +206,9 @@ class Cam(object):
 
 
 
-path = './' # the saving path for snapshots    
-prefix = "img_" # the prefix for saved files
+path = '.' # the saving path for snapshots    
+prefix = "img" # the prefix for saved files
+
 root = tk.Tk()
 root.title("Camera GUI")
 gui = MainWin(root)
